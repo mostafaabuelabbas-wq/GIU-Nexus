786e61e7ba1e5a0473b7ed192934e34d609d10bc
Mostafa Abuelabbas <mostafa.abuelabbas@gmail.com>
2026-05-20 20:57:04 +0300

    Delete Giu nexus (4)/src/components/Spinner.jsx

--- Giu nexus (4)/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Spinner({ size = 'md', white = false }) {
-  const sz = { sm: 'w-4 h-4 border-2', md: 'w-5 h-5 border-2', lg: 'w-8 h-8 border-[3px]' }[size]
-  const color = white
-    ? 'border-white/25 border-t-white'
-    : 'border-[#161310]/15 border-t-[#161310]'
-  return (
-    <div className={`${sz} ${color} rounded-full animate-spin flex-shrink-0`} />
-  )
-}
